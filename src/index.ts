28b311fb6504d279b1827d7ea4ceb589190cd75c
carraes <carraeshb@gmail.com>
2022-12-30 10:25:06 -0300

    feat: login route, add packages

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('teste')
+import express from 'express'
+import colors from 'colors'
+import { router } from './routes/loginRoutes'
+
+colors.enable()
+
+const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(router)
+
+app.listen(3000, () => {
+  console.log('Listening: http://localhost:3000/'.green.bold)
+})
